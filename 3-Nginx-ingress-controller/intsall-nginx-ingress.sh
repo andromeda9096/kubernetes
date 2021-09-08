62266250d45bd8035c44b5d497ec2d132b32148a
trungnguyen <andromeda9096@gmail.com>
2021-09-08 09:39:58 +0700

    Add files via upload

---
+++ 3-Nginx-ingress-controller/intsall-nginx-ingress.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/andromeda9096/Kubernetes.git \
+&& cd ./Kubernetes/3-Nginx-ingress-controller \
+&& helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx \
+&& helm repo update && kubectl create ns nginx-ingress \
+&& helm install my-nginx-controller -n nginx-ingress ingress-nginx/ingress-nginx -f nginx-controller.yaml
