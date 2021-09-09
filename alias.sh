16c9519da69b26cfcee6fa00e0f0a06da685bc1a
trungnguyen <andromeda9096@gmail.com>
2021-09-09 08:47:47 +0700

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -4,4 +4,4 @@
 alias k='kubectl'
 alias ns='kubectl config set-context --current --namespace'
 alias kurrent='kubectl config view --minify -o "jsonpath={..namespace}" | xargs -I %s echo "Current Namespace: %s"'
-alias nodetop='k get nodes | grep Ready | cut -d" " -f1 | xargs kubectl describe node | grep -E "Name: |cpu |memory "'  
+alias nodetop='k get nodes | grep Ready | cut -d" " -f1 | xargs kubectl describe node | grep -E "Name: |cpu |memory "'
